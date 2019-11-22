7a5f6ceee73975b0829000098979276e4b74fe42
user <user@DESKTOP-UL2U1T2>
2019-11-22 14:15:48 +0900

    폭탄 배열에 담기 성공

--- src/yut/model/vo/Mine.java
+++ src/yut/model/vo/Mine.java
@@ -1,15 +1,37 @@
 package yut.model.vo;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import yut.view.GameMap;
 
 public class Mine extends Item {
-    public Mine(int grid, JLabel mine) {
-        //아이템 부모 클래스에 가격 10송편과 설치 하는곳에 grid값과 이름을 보냄
-        super(10,/* grid,*/ "지뢰");
-        // GameMap gm = new GameMap();
-        System.out.println();
+    private JLabel sogplb;
+    
+    public Mine(int price, int grid, JPanel panel) {
+       // super(price, sogplb,"폭탄");
 
-}
+        //마인 객체 생성 및 ui 생성
+        int x = MapGrid.GRIDS.get(grid).x;
+        int y = MapGrid.GRIDS.get(grid).y;
+
+        //송편 표시 라벨
+        Image sogp = new ImageIcon("mini/폭탄.PNG").getImage().getScaledInstance(20, 20, 0);
+        JLabel sogplb = new JLabel(new ImageIcon(sogp));
+        sogplb.setSize(20, 20);
+        sogplb.setLocation(x, y);
+
+        System.out.println("호출");
+
+        panel.add(sogplb);
+        panel.revalidate();
+        panel.repaint();
+        panel.setComponentZOrder(sogplb, 0);
+
+
+    }
+
 }
